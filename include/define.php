0a2c2e677b2e8bc33a72226b101f3265046822a7
Ted <wanganlin@ecmoban.com>
2014-11-28 11:31:13 +0800

    格式化文件格式

--- include/define.php
+++ include/define.php
@@ -17,10 +17,10 @@
 defined('IN_ECTOUCH') or die('Deny Access');
 /* 默认模块 */
 defined('BIND_MODULE') or define('BIND_MODULE', 'Index');
-/* 调试模式 */
-define('APP_DEBUG', true);
 /* 项目名称 */
 define('APP_NAME', 'ECTouch');
+/* 调试模式 */
+define('APP_DEBUG', true);
 /* 项目根目录 */
 define('ROOT_PATH', realpath(dirname(__FILE__) . '/../') . '/');
 /* 核心目录 */
@@ -32,14 +32,14 @@ define('DATA_PATH', ROOT_PATH . 'data/');
 /* 插件目录 */
 define('ADDONS_PATH', ROOT_PATH . 'plugins/');
 /* 项目目录 */
-define('APP_PATH', TOUCH_PATH . 'modules/'. ucfirst(BIND_MODULE) .'/');
+define('APP_PATH', TOUCH_PATH . 'modules/' . BIND_MODULE . '/');
 /* 项目类库目录 */
 define('LIB_PATH', APP_PATH);
 /* 模板目录 */
 define('TMPL_PATH', APP_PATH . 'View/');
 /* 运行时目录 */
-define('RUNTIME_PATH', DATA_PATH . 'cahce/'. ucfirst(BIND_MODULE) .'/');
+define('RUNTIME_PATH', DATA_PATH . 'caching/' . BIND_MODULE . '/');
 /* 日志目录 */
-define('LOG_PATH', DATA_PATH . 'log/'. ucfirst(BIND_MODULE) .'/');
+define('LOG_PATH', DATA_PATH . 'logs/' . BIND_MODULE . '/');
 /* 静态缓存目录 */
 define('HTML_PATH', RUNTIME_PATH . 'Html/');
