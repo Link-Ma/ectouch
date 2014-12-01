925e0d07cfbc88698101fea66f3d5bb82f630dd0
Ted <wanganlin@ecmoban.com>
2014-12-01 10:32:19 +0800

    更新ecs类中的方法

--- include/common/Common/functions.php
+++ include/common/Common/functions.php
@@ -1,12 +1,25 @@
 <?php
 
 /**
- * 密码编译方法;
+     * 将指定的表名加上前缀后返回
+     *
+     * @access  public
+     * @param   string      $str        表名
+     *
+     * @return  string
+     */
+function table($str)
+{
+    return '`' . $this->db_name . '`.`' . $this->prefix . $str . '`';
+}
+
+/**
+ * ECSHOP 密码编译方法;
  *
- * @access  public
- * @param   string      $pass       需要编译的原始密码
+ * @access public
+ * @param string $pass 需要编译的原始密码
  *
- * @return  string
+ * @return string
  */
 function compile_password($pass)
 {
@@ -17,13 +30,14 @@ function compile_password($pass)
  * 取得当前的域名
  *
  * @access public
- *        
+ *
  * @return string 当前的域名
  */
 function get_domain()
 {
     /* 协议 */
-    $protocol = http();
+    $protocol = $this->http();
+    
     /* 域名或IP地址 */
     if (isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
         $host = $_SERVER['HTTP_X_FORWARDED_HOST'];
@@ -52,30 +66,72 @@ function get_domain()
 }
 
 /**
- * 获得当前环境的 URL 地址
+ * 获得 ECSHOP 当前环境的 URL 地址
  *
  * @access public
- *        
+ *
  * @return void
  */
 function url()
 {
     $curr = strpos(PHP_SELF, ADMIN_PATH . '/') !== false ? preg_replace('/(.*)(' . ADMIN_PATH . ')(\/?)(.)*/i', '\1', dirname(PHP_SELF)) : dirname(PHP_SELF);
+    
     $root = str_replace('\\', '/', $curr);
+    
     if (substr($root, - 1) != '/') {
         $root .= '/';
     }
-    return get_domain() . $root;
+    
+    return $this->get_domain() . $root;
 }
 
 /**
- * 获得当前环境的 HTTP 协议方式
+ * 获得 ECSHOP 当前环境的 HTTP 协议方式
  *
  * @access public
- *        
+ *
  * @return void
  */
 function http()
 {
     return (isset($_SERVER['HTTPS']) && (strtolower($_SERVER['HTTPS']) != 'off')) ? 'https://' : 'http://';
+}
+
+/**
+ * 获得数据目录的路径
+ *
+ * @param int $sid
+ *
+ * @return string 路径
+ */
+function data_dir($sid = 0)
+{
+    if (empty($sid)) {
+        $s = 'data';
+    } else {
+        $s = 'user_files/';
+        $s .= ceil($sid / 3000) . '/';
+        $s .= $sid % 3000;
+    }
+    return $s;
+}
+
+/**
+ * 获得图片的目录路径
+ *
+ * @param int $sid
+ *
+ * @return string 路径
+ */
+function image_dir($sid = 0)
+{
+    if (empty($sid)) {
+        $s = 'images';
+    } else {
+        $s = 'user_files/';
+        $s .= ceil($sid / 3000) . '/';
+        $s .= ($sid % 3000) . '/';
+        $s .= 'images';
+    }
+    return $s;
 }
