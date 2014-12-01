c29cddf9441519b43b15aec7320b9a7883effe40
Ted <wanganlin@ecmoban.com>
2014-12-01 15:38:47 +0800

    增加初始化方法

--- include/common/Controller/Controller.class.php
+++ include/common/Controller/Controller.class.php
@@ -6,10 +6,16 @@ class Controller extends Action
     public function __construct()
     {
         parent::__construct();
-        
+    }
+    
+    protected function _initialize(){
         // 安装检测
         if (! file_exists(ROOT_PATH . 'data/install.lock') && ! defined('NO_CHECK_INSTALL')) {
             redirect(U('install/index'));
         }
+        header("Content-type: text/html; charset=".EC_CHARSET);
+        /* 载入系统参数 */
+        C('setting', load_config());
+        
     }
 }
