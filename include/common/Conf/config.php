99fb49954c68a46c698dcf84098fdbed981d9342
Ted <wanganlin@ecmoban.com>
2014-12-01 13:17:02 +0800

    增加trace信息显示

--- include/common/Conf/config.php
+++ include/common/Conf/config.php
@@ -2,6 +2,7 @@
 
 $database = require DATA_PATH . 'config.php';
 $conf = array(
+    'SHOW_PAGE_TRACE'=>true,
     'TMPL_FILE_DEPR' => '_',
     'APP_AUTOLOAD_PATH' => TOUCH_PATH.'vendor/',
     'LOAD_BASE_EXT_FILE' => 'constant,lib_time,lib_base,lib_common'
