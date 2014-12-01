fdc998fc1caab6379ca957abdcc04a08a4670ca1
Ted <wanganlin@ecmoban.com>
2014-12-01 15:38:36 +0800

    关闭静态缓存

--- include/modules/Index/Conf/config.php
+++ include/modules/Index/Conf/config.php
@@ -3,12 +3,19 @@ return array(
     'VIEW_PATH' => ROOT_PATH . 'themes/',
     'DEFAULT_THEME' => 'default',
     'TMPL_ENGINE_TYPE' => 'Smarty',
-    'HTML_CACHE_ON' => true,
+    'HTML_CACHE_ON' => false,
     'HTML_CACHE_RULES'=> array(
         'index:index'=>array('{$_SERVER.REQUEST_URI|md5}'),
     ),
+    /*
     'TMPL_ACTION_ERROR'     => DATA_PATH.'assets/system/message.tpl', // 默认错误跳转对应的模板文件
     'TMPL_ACTION_SUCCESS'   => DATA_PATH.'assets/system/message.tpl', // 默认成功跳转对应的模板文件
     'TMPL_EXCEPTION_FILE'   => DATA_PATH.'assets/system/exception.tpl'
+    */
 );
+/*
+require(ROOT_PATH . 'includes/lib_main.php');
+require(ROOT_PATH . 'includes/lib_insert.php');
+require(ROOT_PATH . 'includes/lib_goods.php');
+require(ROOT_PATH . 'includes/lib_article.php');
 ?>
